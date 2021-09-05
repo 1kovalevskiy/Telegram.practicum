255a70ed1523596bdf1435822b93a90055d5cb15
Nikita Kovalevskiy <koval701@gmail.com>
2021-09-05 22:14:44 +0700

    fix

--- homework.py
+++ homework.py
@@ -8,13 +8,17 @@ import logging
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRAKTIKUM_TOKEN = os.environ.get('PRAKTIKUM_TOKEN')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 _log_format = '%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+
+SLEEP_TIME = 20 * 60
 
 
 class TelegramLoggingHandler(logging.StreamHandler):
@@ -57,30 +61,41 @@ logger = get_logger(__name__)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        raise ValueError('Нет такого поля')
     if homework['status'] == 'rejected':
         logger.info('Домашку нужно доделать')
         verdict = 'К сожалению, в работе нашлись ошибки.'
     elif homework['status'] == 'reviewing':
         logger.info('Домашку начали проверять')
         return f'Домашка {homework_name} взята в работу'
-    else:
+    elif homework['status'] == 'approved':
         logger.info('С домашкой все ОК')
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homeworks(current_timestamp=1581604970):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    # if current_timestamp > int(time.time()):
+    #     current_timestamp = int(time.time())
+    # не проходит проверку pytest
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
+    response = requests.get(URL, headers=HEADERS, params=payload)
+    if response.status_code != 200:
+        raise ValueError(f'Ошибка ответа сервера {response.status_code}')
     logger.info('Информация о домашке получена')
-    return homework_statuses.json()
+    parsed_response = response.json()
+    if 'error' in parsed_response.keys():
+        raise ValueError('Ошибка ответа сервера')
+    return parsed_response
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    try:
+        return bot.send_message(CHAT_ID, message)
+    except Exception as e:
+        logger.warning(f'Не отправляется сообщение "{e}"')
 
 
 def main():
@@ -95,7 +110,7 @@ def main():
             logger.info('Список домашек получен')
             if homeworks.get('homeworks') == []:
                 logger.info('нету домашних работ')
-                time.sleep(20 * 60)
+                time.sleep(SLEEP_TIME)
                 continue
             else:
                 homework = homeworks['homeworks'][0]
@@ -105,11 +120,11 @@ def main():
                 verdict = parse_homework_status(homework)
                 response = send_message(verdict)
                 logger.info(response)
-            time.sleep(20 * 60)
+            time.sleep(SLEEP_TIME)
 
         except Exception as e:
             logger.error(f'Бот упал с ошибкой: {e}')
-            time.sleep(5 * 60)
+            time.sleep(SLEEP_TIME)
     else:
         send_message('Отслеживание завершено')
         logger.debug('Отслеживание завершено')
